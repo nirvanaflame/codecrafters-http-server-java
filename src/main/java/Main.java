558752263fb3d6426ab2daadc69415b6865d5c28
nf <sergey.putik@mail.ru>
2024-04-10 20:28:02 +0200

    pass 2st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,35 @@
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
 
-     try {
-       serverSocket = new ServerSocket(4221);
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+        try {
+            serverSocket = new ServerSocket(4221);
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept(); // Wait for connection from
+            // client.
+
+            InputStream inputStream = clientSocket.getInputStream();
+
+            DataOutputStream dataOutputStream = new DataOutputStream(
+                    clientSocket.getOutputStream());
+
+            byte[] message = "HTTP/1.1 200 OK\r\n\r\n".getBytes(
+                    StandardCharsets.UTF_8);
+            dataOutputStream.write(message);
+
+            System.out.println("accepted new connection");
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
