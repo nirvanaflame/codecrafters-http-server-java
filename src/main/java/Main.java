21aa7f6f431e5f5746ece97f649492409ced4577
nf <sergey.putik@mail.ru>
2024-04-10 22:18:19 +0200

    pass 5st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -20,11 +23,7 @@ public class Main {
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept();
 
-            InputStream inputStream = clientSocket.getInputStream();
-            String[] in = convertToString(inputStream);
-
-            String path = in[1];
-            byte[] response = createResponse(path);
+            byte[] response = createResponse(clientSocket.getInputStream());
 
             OutputStream outputStream = clientSocket.getOutputStream();
             outputStream.write(response);
@@ -35,26 +34,58 @@ public class Main {
         }
     }
 
-    private static byte[] createResponse(String path) {
+    private static byte[] createResponse(InputStream inputStream) throws IOException {
+        String[] request = lines(inputStream);
+
+        String path = request[0].split(" ")[1];
         if (path.startsWith("/echo")) {
             String text = path.split("/echo/")[1];
 
-            return ("HTTP/1.1 200 OK\r\n" +
-                    "Content-Type: text/plain\r\n" +
-                    "Content-Length: %d\r\n".formatted(text.length()) +
-                    "\r\n" +
-                    "%s".formatted(text))
-                    .getBytes(UTF_8);
+            return createOk(text);
+        }
+
+        if (path.startsWith("/user-agent")) {
+            String userAgent = headers(request).get("User-Agent");
+
+            return createOk(userAgent);
         }
 
         return path.equals("/")
-                ? "HTTP/1.1 200 OK\r\n\r\n".getBytes(UTF_8)
+                ? createOk("")
                 : "HTTP/1.1 404 Not Found\r\n\r\n".getBytes(UTF_8);
     }
 
-    private static String[] convertToString(InputStream inputStream) throws IOException {
+    private static byte[] createOk(String text) {
+        String response;
+        if (text.isBlank()) {
+            response = "HTTP/1.1 200 OK\r\n\r\n";
+        } else {
+            response = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: text/plain\r\n" +
+                    "Content-Length: %d\r\n".formatted(text.length()) +
+                    "\r\n" +
+                    "%s".formatted(text);
+        }
+
+        return response.getBytes(UTF_8);
+    }
+
+    private static Map<String, String> headers(String[] in) {
+        Map<String, String> headers = new HashMap<>();
+        for (int i = 1; i < in.length; i++) {
+            String str = in[i];
+
+            if (str.equals("\r\n")) {break;}
+
+            String[] kv = str.split(": ");
+            headers.put(kv[0], kv[1]);
+        }
+        return headers;
+    }
+
+    private static String[] lines(InputStream inputStream) throws IOException {
         return new BufferedReader(new InputStreamReader(inputStream))
                 .readLine()
-                .split(" ");
+                .split("\r\n");
     }
 }
